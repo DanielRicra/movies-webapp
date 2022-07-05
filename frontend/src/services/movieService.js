e2115ea6df2f4712ad2af1b9ad2d14decc7ff93a
TSdnl13 <daniel.int@outlook.com>
2022-07-05 00:11:44 -0500

    add fin latest movies-request with axios

--- frontend/src/services/movieService.js
+++ frontend/src/services/movieService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const latestMoviesUrl = "https://api.themoviedb.org/3/movie/upcoming?api_key=de101b94f5d8dad0212f558edc5c9db1&language=en-US&page="
 
-export const findLatestMovies = (page = 1 ) => {
+export const findLatestMovies = async (page = 1 ) => {
    const response = await axios.get(`${latestMoviesUrl}${page}`)
-   return response
+   return await response.data
 }
