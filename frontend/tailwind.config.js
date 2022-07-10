9b5f8ca7c0231645360a8cfc1c41149dbf30ad79
TSdnl13 <daniel.int@outlook.com>
2022-07-09 23:13:42 -0500

    add: custom font family base foor our web app in tailwind.config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    theme: {
+      fontFamily: {
+        sans: ['ui-sans-serif', 'system-ui'],
+        serif: ['ui-serif', 'Georgia'],
+        mono: ['ui-monospace', 'SFMono-Regular'],
+        base: ['Fira Sans', 'sans-serif'],
+      }
+    }
   },
   plugins: [require("daisyui")],
   daisyui: {
