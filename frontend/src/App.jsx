d4f0537c0e6b69a567ae72d4b7ac97824312c6cf
TSdnl13 <daniel.int@outlook.com>
2022-07-15 16:16:21 -0500

    add: App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Home from './containers/Home'
+import Nav from './containers/Nav'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import SearchResult from './containers/SearchResult'
+import MovieDetail from './components/movies/MovieDetail'
+import PeopleDetails from './components/people/PeopleDetails'
+import TVDetails from './components/tv/TVDetails'
+import Footer from './containers/Footer'
 
 function App() {
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  )
+   return (
+      <div className="flex flex-col items-start min-h-screen overflow-x-hidden">
+         <Router >
+            <Nav />
+            <Routes>
+               <Route path='/' element={<Home />} />
+               <Route path='/search' element={<SearchResult />} />
+               <Route path='/movie/:id' element={<MovieDetail />} />
+               <Route path='/people/:id' element={<PeopleDetails />} />
+               <Route path='/tv/:id' element={<TVDetails />} />
+            </Routes>
+            <Footer />
+         </Router>
+      </div>
+   )
 }
 
 export default App
